c4a964bc0d715ff01109c762580ba2c5f4d25df5
eswaribala <parameswaribala@gmail.com>
2021-11-22 16:50:34 +0530

    updated

---
+++ invapi/src/main/java/com/eric/invapi/InvapiApplication.java
@@ -0,0 +1,13 @@
+package com.eric.invapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InvapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InvapiApplication.class, args);
+	}
+
+}
