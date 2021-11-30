6edb7981f6c15fe25888e2466aa7085bc775233e
eswaribala <parameswaribala@gmail.com>
2021-11-30 15:52:48 +0530

    updated

---
+++ camundacustomerapi/src/main/java/com/eric/camundacustomerapi/RegistrationLogger.java
@@ -0,0 +1,33 @@
+package com.eric.camundacustomerapi;
+
+import java.util.logging.Logger;
+
+import org.camunda.bpm.engine.delegate.DelegateExecution;
+import org.camunda.bpm.engine.delegate.JavaDelegate;
+import org.springframework.stereotype.Component;
+
+/**
+ * This is an easy adapter implementation
+ * illustrating how a Java Delegate can be used
+ * from within a BPMN 2.0 Service Task.
+ */
+@Component("registrationLogger")
+public class RegistrationLogger implements JavaDelegate {
+ 
+  private final Logger LOGGER = Logger.getLogger(RegistrationLogger.class.getName());
+  
+  public void execute(DelegateExecution execution) throws Exception {
+    
+    LOGGER.info("\n\n  ... Registration LoggerDelegate invoked by "
+            + "activityName='" + execution.getCurrentActivityName() + "'"
+            + ", activityId=" + execution.getCurrentActivityId()
+            + ", processDefinitionId=" + execution.getProcessDefinitionId()
+            + ", processInstanceId=" + execution.getProcessInstanceId()
+            + ", businessKey=" + execution.getProcessBusinessKey()
+            + ", executionId=" + execution.getId()
+            + ", variables=" + execution.getVariables()
+            + " \n\n");
+    
+  }
+
+}
